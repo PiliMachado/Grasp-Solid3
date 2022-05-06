4d076d1b9fe0228c0d4710f6630cf0cd4a8246ac
PiliMachado <88661591+PiliMachado@users.noreply.github.com>
2022-05-06 10:05:21 -0300

    Comentario de principios

--- src/Library/IPrinter.cs
+++ src/Library/IPrinter.cs
@@ -3,9 +3,11 @@ using System;
 namespace Full_GRASP_And_SOLID.Library
 {
     /// <summary>
-    /// Basandonos en los Principios Polimorfismo y LSP.
-    /// En vez de un condicional, creamos una interfaz con la operacion PrintRecipe, la cual va a ser implementada por las clases
-    /// ConsolePrinter y FilePrinter.
+    /// Basandome en los Principios Polimorfismo y LSP.
+    /// En vez de un condicional, creo una interfaz con la operacion PrintRecipe,
+    /// la cual va a ser implementada por las clase ConsolePrinter y FilePrinter. 
+    /// De querer añadirse una nueva forma de impresion se crea una
+    /// clase y se utiliza esta interfaz en dicha clase
     /// </summary>
     public interface IPrinter
     {
